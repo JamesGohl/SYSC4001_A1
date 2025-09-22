e9dd7ce326ea565fb830d22c0411edd8a799937c
James Gohl <james8hg@gmail.com>
2025-09-22 14:25:45 -0400

    end of lab testing things out

--- interrupts.cpp
+++ interrupts.cpp
@@ -29,7 +29,7 @@ int main(int argc, char** argv) {
         auto [activity, duration_intr] = parse_trace(trace);
 
         /******************ADD YOUR SIMULATION CODE HERE*************************/
-
+        std::cout << 123 ;
 
 
         /************************************************************************/
